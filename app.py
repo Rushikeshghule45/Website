61f7453cd4a2ec40f8cf2ae66c2524034d76cf4e
Rushikeshghule45 <rhushikesh.ghule1998@gmail.com>
2025-11-23 13:10:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-## Title of the aplication
-st.title("Hello Ketan")
+st.title("Rhushi MOTORS")
+st.header("Welcome to HomePage")
+st.subheader("This is a website to showcase different BIKES")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
+st.write("\n")
 
-## Diplay a Simple Text
-st.write("This is a simple text")
-
-##create a simple Dataframe
-
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4,1, 2, 3, 4],
-    'second column': [10, 20, 30, 40,10, 20, 30, 40]
-})
-
-
-## Display the Dataframe
-st.write("Here is the dataframe")
-st.write(df)
+#with st.container():
+#    st.subheader("bajaj pulsar NS160")
+#    st.image("artifacts\images\Bajaj Pulsar NS160.jpg")
+#    st.subheader("KTM 390 DUKE")
+#    st.image("artifacts\images\KTM 390 DUKE.webp")
+#    st.subheader("R15")
+#    st.image("artifacts\images\R15.jpg")
+#    st.subheader("Royal Enfield")
+#    st.image("artifacts\images\Royal Enfield.jpg")
 
 
-##create a line chart
+col1, col2, col3 = st.columns([3, 3, 3],gap="medium",vertical_alignment='center',border=True)
+col1.subheader("Bajaj Pulsar NS160")
+col1.image("artifacts\images\Bajaj Pulsar NS160.jpg")
+col2.subheader("KTM 390 DUKE")
+col2.image("artifacts\images\KTM 390 DUKE.webp")
+col3.subheader("R15")
+col3.image("artifacts\images\R15.jpg")  
+col4.subheader("Royal Enfield")
+col4.image("artifacts\images\Royal Enfield.jpg") 
+st.selectbox("Pick one", ["None","Bajaj pulsar NS160", "KTM 390 DUKE","R15","Royal Enfield"],placeholder="None")
 
-chart_data=pd.DataFrame(
-    np.random.randn(20,3),columns=['a','b','c']
-)
-st.line_chart(chart_data)
+price = st.slider("Enter Your Budget", min_value=50000, max_value=5000000,step=10000,value=100000)
+st.write(f"Your Selected Budget is :- {price}")
+st.text_input("Enter Your name")
+st.text_input("Enter your Contact no")
+st.text_input("Enter your Email Id")
+st.text_area("Enter your full address",placeholder="thane")
+if st.button("Submit"):
+    st.switch_page('pages/form_feedback.py')
---
+++ pages/feedback.py
@@ -0,0 +1,4 @@
+import streamline as st
+
+st.header("Thank you.........")
+st.subheader("your Enquiry has been submited successfully")
